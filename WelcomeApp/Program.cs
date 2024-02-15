6e6143480bcd6110221c8b6267ce1a5a262b2b8c
PuneethMurali <65094114+PuneethMurali@users.noreply.github.com>
2024-02-15 12:21:43 +0530

    Final commit

---
+++ WelcomeApp/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using WelcomeApp;
+
+using WelcomeApp
+
+Console.WriteLine("Hello, World!");
+SignIn obj = new SignIn();
+Console.WriteLine(obj.Authenticate("admin", "admin@1256"));
